63099fb1744f962bbf8993e15f80d4627aa6e05a
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-04-21 21:19:30 +0800

    Create ex1-1.cpp

---
+++ ex1-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    printf("hello my friend!\n");
+    printf("my age is %d year-old.",17);
+    printf("Nice to see you!\n");
+    printf("I'm Anna. I am 7 years old.");
+
+    return 0;
+}
