81f2fbc6f5ff61484324241bdffb436aa8f19631
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-05-05 20:41:34 +0800

    Create ex3-3.cpp

---
+++ ex3-3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    
+    a=6;
+    a+=7;
+    printf("%d \n",a);
+    a-=7;
+    printf("%d \n",a);
+    a*=7;
+    printf("%d \n",a);
+    a/=7;
+    printf("%d \n",a);
+    a%=5;
+    printf("%d \n",a);
+    
+    
+    b=8;
+    b&=5;
+    printf("%d \n",b);
+    b|=5;
+    printf("%d \n",b);
+    b^=5;
+    printf("%d \n",b);
+    
+    
+
+    return 0;
+    
+
+
+    
+}
