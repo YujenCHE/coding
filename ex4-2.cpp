9371b0ad3c5a6d0f507c132441802ae8386ce7a8
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-05-12 19:51:00 +0800

    Create ex4-2.cpp

---
+++ ex4-2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    int x;
+    scanf("%d",&x);
+    
+    if (x>5)      //if()不用分號
+    printf("x>5\n");
+    else         //else不用分號
+    printf("x<=5");
+    
+    
+    if (x<10)    //if()不用分號
+    printf("x<10\n");
+    else         //else不用分號
+    printf("x>=10");
+    
+    return 0;
+}
