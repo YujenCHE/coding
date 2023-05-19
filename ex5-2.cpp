3e9ce6964b38f1b15a71e2650a9d9c9fd779d6f1
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-05-19 20:14:21 +0800

    Create ex5-2.cpp
    原本成功的

---
+++ ex5-2.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main(){
+    char op;
+    double first,second;
+    
+    printf("Please enter + - * /");
+    scanf("%c", &op);
+    printf("Please enter 2 operands");
+    scanf("%lf %lf", &first, &second);
+    
+    switch(op){
+        case'+':    //case要用冒號:
+        printf("%.1lf + %.1lf = %.1lf ",first,second,first + second);
+        break;//用break跟其他事件分開
+        
+        case'-':
+        printf("%.1lf - %.1lf = %.1lf",first,second,first - second);
+        break;
+        
+        case'*':
+        printf("%.1lf * %.1lf = %.1lf",first,second,first * second);
+        break;
+        
+        case'/':
+        //以下是chatGDP加的
+        if (second != 0) {
+                printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+        } 
+        else {
+                printf("Error 不能用0除!");  // 處理除以零的情況
+        }
+        break;
+        
+        default:
+            printf("Error: 無效運算子");  // 處理無效運算子的情況
+            break;
+        
+        
+    }
+    return 0;
+}
