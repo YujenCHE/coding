9eac3df327f4a3a57d2e8ad2b575690d7b5a9c99
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-05-19 20:27:34 +0800

    Create ex5-3.cpp

---
+++ ex5-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i;
+    
+    for(i=0;i<10;i++){//()內條件中間用分號;隔開
+        printf("%d * %d = %d\n",i,i,i*i);
+    }
+    
+    cout<<"End";
+    return 0;
+}
