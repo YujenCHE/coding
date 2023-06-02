116db69e510104e379bdfe6c1101bf732b545f90
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-02 20:26:37 +0800

    Create ex6-5.cpp

---
+++ ex6-5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int temp(int a, int b){
+    b=a*1.8+32;
+    return b;
+}
+
+int main(){
+    int a,b;
+    
+    printf("請輸入攝氏 \n");
+    scanf("%d", &a);
+    printf("攝氏%d˚C 是華氏%d˚F",a,temp(a,b));
+    return 0;
+}
