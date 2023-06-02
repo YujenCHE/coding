72b85fb19f68c820e0fa376a783d33a65db27a0b
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-02 16:34:08 +0800

    Create ASCIIcode.cpp
    做到一半

---
+++ ASCIIcode.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin>>str;
+    int i,s = str.size();
+    
+    for(i=0;i<s;i++){
+        int trans = int(str[i]);//把字串的字變成ASCII code的數字
+    }
+    
+    return 0;
+}
