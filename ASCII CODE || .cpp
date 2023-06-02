b1763305e5067cc54e5803427063f16438c4dfe0
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-02 14:28:48 +0800

    Create ASCII CODE || .cpp
    用printf的版本
    %c 是字母
    %d 是數字

---
+++ ASCII CODE || .cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    char c = 'B';//一ˋ定要用''包起來
+    printf("The ASCII CODE of %d is %c. \n",c,c);
+    printf("The ASCII CODE of %c is %d. \n",c,c);
+    //%c 是原本的字母
+    //%d 是轉換成數字的
+    return 0;
+}
