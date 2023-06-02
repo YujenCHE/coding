959aea9cf4ec9c131c7b61a1f850ccbd304186de
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-02 14:22:30 +0800

    Create ASCII CODE | .cpp

---
+++ ASCII CODE | .cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    char c = 'B';//一ˋ定要用''包起來
+    cout<<"the ASCII CODE of "<<c<<" is "<<int(c);
+    //用int(c) 就能把字母變成ASCII CODE的碼
+    return 0;
+}
