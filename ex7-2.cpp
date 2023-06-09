f579ae1ac9b55e9195ed921f626e337d4799302a
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-09 19:50:11 +0800

    Create ex7-2.cpp
    0! = 1
    1! = 1
    2! = 2
    3! = 6
    4! = 24
    5! = 120

---
+++ ex7-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+/***
+ n階乘的程式
+ n!=n*(n-1)*(n-2)*...*3*2*1
+ ***/
+#if 0//1,2,3都有一樣的結果
+int f(int n){
+    if(n<=1){
+        return 1;
+    }else{
+        return n*f(n-1);
+    }
+}
+
+#else
+int f(int n){
+    int val = 1;
+    int i;
+    for(i=2;i<=n;i++){//階乘n!=n*(n-1)*(n-2)*...*3*2*1
+        val=val*i;
+    }
+    return val;
+}
+
+#endif
+int main()
+{   int num;
+    for(num=0;num<=5;num++){
+    cout<<num<<"! = "<<f(num)<<endl;
+    }
+    return 0;
+}
