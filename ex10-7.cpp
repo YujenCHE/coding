f9fafe3f065134d20207974c20414173ce0bdd0a
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-30 21:06:02 +0800

    Create ex10-7.cpp

---
+++ ex10-7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    char hello[] = "Hello, my friend. How are you?";
+    int i;
+    for(i=0;hello[i]!='\0';i++)
+    {
+        i=i;
+    }
+    
+    printf("Length of string is %d\n", i);
+    
+    return 0;
+}
