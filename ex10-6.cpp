d48f1eeef7a08a33e7bf1ee54789d6e426a93632
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-30 20:49:34 +0800

    Create ex10-6.cpp

---
+++ ex10-6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    char name[240];
+    printf("Enter name : ");
+    gets(name);
+    printf("Name : ");
+    puts(name);
+    
+    return 0;
+}
